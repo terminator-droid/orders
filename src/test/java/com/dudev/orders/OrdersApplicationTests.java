cbdcde4572bc1b32ab6271a0f9199b12d3d12875
Даниил <daniil@MacBook-Air-Daniil.local>
2025-12-27 23:47:30 +0800

    Initial commit

---
+++ src/test/java/com/dudev/orders/OrdersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dudev.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class OrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/dudev/orders/TestOrdersApplication.java
@@ -0,0 +1,11 @@
+package com.dudev.orders;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(OrdersApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
